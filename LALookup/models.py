9ac6d6b077b0dc0536b90f408914e4b2bb93c8d9
Matthew Wollenweber <mjw@insomniac.tech>
2025-03-17 22:10:17 -0500

    wip - requirements

--- LALookup/models.py
+++ LALookup/models.py
@@ -10,9 +10,9 @@ class Person(models.Model):
     personalEmail = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     homePhone = models.CharField(max_length=200, blank=True, null=True)
     mobile = models.CharField(max_length=200, blank=True, null=True)
-    #status = models.CharField(max_length=200, blank=True, null=True)
     photoURL = models.CharField(max_length=200, blank=True, null=True)
     gender = models.CharField(max_length=200, blank=True, null=True)
+    ethnicity = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     party = models.CharField(max_length=200, blank="U", null=True, db_index=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True)
@@ -22,6 +22,7 @@ class Person(models.Model):
     linkedin = models.CharField(max_length=200, blank=True, null=True)
     youtube = models.CharField(max_length=200, blank=True, null=True)
     website = models.CharField(max_length=200, blank=True, null=True)
+    parish = models.CharField(max_length=200, blank=True, null=True, db_index=True)
 
     def __str__(self):
         return self.fullname
@@ -98,9 +99,8 @@ class API(models.Model):
 #     year = models.IntegerField(default=0)
 
 
-class SoSElectedOfficial(models.Model):
+class SoSElectedOfficial(Person):
     id = models.AutoField(primary_key=True)
-    created = models.DateTimeField(auto_now=True, editable=True, db_index=True)
     officeTitle = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     officeDescription = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     officeAddress1 = models.CharField(max_length=200, blank=True, null=True, db_index=True)
@@ -108,19 +108,12 @@ class SoSElectedOfficial(models.Model):
     officeCity = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     officeZip = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     officePhone = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    parish = models.CharField(max_length=200, blank=True, null=True, db_index=True)
+    officeEmail = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     candidateAddress1 = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     candidateAddress2 = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     candidateCity = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     candidateZip = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    ethnicity = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    gender = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    party = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    email = models.CharField(max_length=200, blank=True, null=True, db_index=True)
     commissionedDate = models.DateTimeField(editable=True, db_index=True, blank=True, null=True)
     expirationDate = models.DateTimeField(editable=True, db_index=True, blank=True, null=True)
     officeLevel = models.IntegerField(default=0, db_index=True)
     candidateName = models.CharField(max_length=200, blank=True, null=True, db_index=True)
-    first_name = models.CharField(max_length=200, db_index=True)
-    last_name = models.CharField(max_length=200, db_index=True)
-    phone = models.CharField(max_length=200, blank=True, null=True, db_index=True)
