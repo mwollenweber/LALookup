71508ba28a2b2adef64d7bfca94f573f5fcaa8a8
Matthew Wollenweber <mjw@insomniac.tech>
2025-05-20 18:43:32 -0500

    disable debug and randomize secret_key

--- LALookup/admin.py
+++ LALookup/admin.py
@@ -4,18 +4,18 @@ from .models import Legislator, SoSElectedOfficial, Request
 
 class RequestAdmin(admin.ModelAdmin):
     model = Request
-    search_fields = ['remote_address', 'response_code', 'endpoint', 'date']
+    search_fields = ["remote_address", "response_code", "endpoint", "date"]
     title = [Request.id, Request.method, Request.response_code, Request.date]
 
 
 class LegislatorAdmin(admin.ModelAdmin):
     model = Legislator
-    search_fields = ['first_name', 'last_name']
+    search_fields = ["first_name", "last_name"]
 
 
 class ElectedOfficialAdmin(admin.ModelAdmin):
     model = SoSElectedOfficial
-    search_fields = ['first_name', 'last_name']
+    search_fields = ["first_name", "last_name"]
 
 
 admin.site.register(Legislator, LegislatorAdmin)
